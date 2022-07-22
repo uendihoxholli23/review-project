56400101cb82cb2bb14d3f77ac7f66f59b3c63ca
Uendi Hoxholli <uendihoxholli33@gmail.com>
2022-07-22 13:37:13 +0300

    First commit

---
+++ server/script.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({ data: {username: "Uendi", surname:"Hoxholli", comment:"Excellent quality"} })
+    console.log(user)
+}
+
+main()
+    .catch(e =>{
+        console.error(e.message)
+    })
+    .finally(async() =>{
+        await prisma.$disconnect()
+    })
+
+    
