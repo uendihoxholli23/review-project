2dd926b6362322981c86125cf5d147ca1bb8a9ec
Uendi Hoxholli <uendihoxholli33@gmail.com>
2022-08-01 15:37:52 +0300

    star rating

--- client/src/Slider.jsx
+++ client/src/Slider.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react'
+import { AiOutlineVerticalRight, AiOutlineVerticalLeft} from "react-icons/ai";
+import StarRating from './StarRating';
 
+ 
 const dishes = [
     "/images/food1.jpg",
     "/images/food2.jpg",
@@ -7,6 +10,7 @@ const dishes = [
 ];
 
 let count = 0;
+let slideInterval;
 export default function Slider(){
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -18,13 +22,22 @@ export default function Slider(){
     }
     useEffect(() => {
         slideRef.current.addEventListener('animationend', removeAnimation);
+        slideRef.current.addEventListener('mouseenter', pauseSlider);
+        slideRef.current.addEventListener('mouseleave', startSlider);
         startSlider();
+        return () => {
+            pauseSlider()
+        }
     }, []);
 
     const startSlider = () => {
-        setInterval(() => {
+     slideInterval = setInterval(() => {
             handleOnNextClick()
         }, 3000);
+    }
+
+    const pauseSlider = () => {
+        clearInterval(slideInterval)
     }
 
     const handleOnNextClick = () => {
@@ -41,13 +54,20 @@ export default function Slider(){
     }
 
     return (
-        <div ref={slideRef} className='w-full select-none'>
-                <div className='aspect-w-16 aspect-h-9'></div>
-            <img src={dishes[currentIndex]} alt="" />
-            <div className='absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center'>
-                <button onClick={handleOnPrevClick}>Previous</button>
-                <button onClick={handleOnNextClick}>Next</button>
+        
+        <div ref={slideRef} className=' select-none'>
+            <div style={{textAlign: 'center', fontSize: 23}}>
+                <h2>Let customers speak for us</h2>
+                <StarRating />
+            </div>
+            <div className='aspect-w-16 aspect-h-3' style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <img src={dishes[currentIndex]} alt="" />
+            </div>
+            <div className='absolute  w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center'>
+                <button className='bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition' onClick={handleOnPrevClick}><AiOutlineVerticalRight size={30}/></button>
+                <button className='bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition' onClick={handleOnNextClick}><AiOutlineVerticalLeft size={30}/></button>
             </div>
         </div>
+      
     )
 }
