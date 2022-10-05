1cd841ab176cd3893314647db875bb67ce529173
Uendi Hoxholli <uendihoxholli33@gmail.com>
2022-10-05 14:43:37 +0300

    useNavigate, passing value to another page

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -28,56 +28,6 @@ function Cards() {
               </div>
             </div>
           </div>
-          <div class="p-4 md:w-1/3">
-            <div class="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center"
-                src="https://dummyimage.com/721x401"
-                alt="blog"
-              ></img>
-              <div class="p-6">
-                <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                  Username and surname
-                </h2>
-                <h1 class="title-font text-lg font-medium text-white mb-3">
-                  Title
-                </h1>
-                <p class="leading-relaxed mb-3">
-                  If you are a bread lover, buy this bread immediately! It's
-                  made with whole grain flour, rich in nutrient but low in
-                  calorie.
-                </p>
-                <button class="bg-transparent hover:bg-teal-500 text-teal-500 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
-                  To Favorites
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="p-4 md:w-1/3">
-            <div class="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center"
-                src="https://dummyimage.com/722x402"
-                alt="blog"
-              ></img>
-              <div class="p-6">
-                <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                  Username and surname
-                </h2>
-                <h1 class="title-font text-lg font-medium text-white mb-3">
-                  Title
-                </h1>
-                <p class="leading-relaxed mb-3">
-                  If you are a bread lover, buy this bread immediately! It's
-                  made with whole grain flour, rich in nutrient but low in
-                  calorie.
-                </p>
-                <button class="bg-transparent hover:bg-teal-500 text-teal-500 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
-                  To Favorites
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </section>
--- client/src/pages/AddReview.jsx
+++ client/src/pages/AddReview.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+
 function AddReview() {
+  const navigate = useNavigate();
   const addReview = (values) => {
     const { username, surname, comment } = values;
     axios
       .post("http://localhost:3000/", { username, surname, comment })
       .then((res) => {
         console.log("Posted!", res);
+        navigate("/");
         window.location.reload();
       })
       .catch((err) => {
@@ -28,78 +31,80 @@ function AddReview() {
   };
 
   return (
-    <Formik
-      initialValues={{ username: "", surname: "", comment: "" }}
-      onSubmit={addReview}
-      validationSchema={Yup.object().shape({
-        username: Yup.string().required("Required"),
-        surname: Yup.string().required("Required"),
-        comment: Yup.string().required("Required"),
-      })}
-    >
-      {(props) => {
-        const {
-          values,
-          touched,
-          errors,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        } = props;
-        return (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username: </label>
-            <input
-              name="username"
-              type="text"
-              placeholder="Enter your username"
-              value={values.username}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.username && touched.username && "error"}
-            />
-            {errors.username && touched.username && (
-              <div className="input-feedback">{errors.username}</div>
-            )}
+    <section class="text-gray-400 bg-gray-900 body-font h-screen">
+      <Formik
+        initialValues={{ username: "", surname: "", comment: "" }}
+        onSubmit={addReview}
+        validationSchema={Yup.object().shape({
+          username: Yup.string().required("Required"),
+          surname: Yup.string().required("Required"),
+          comment: Yup.string().required("Required"),
+        })}
+      >
+        {(props) => {
+          const {
+            values,
+            touched,
+            errors,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          } = props;
+          return (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">Username: </label>
+              <input
+                name="username"
+                type="text"
+                placeholder="Enter your username"
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.username && touched.username && "error"}
+              />
+              {errors.username && touched.username && (
+                <div className="input-feedback">{errors.username}</div>
+              )}
 
-            <label htmlFor="surname">Surname: </label>
-            <input
-              name="surname"
-              type="text"
-              placeholder="Enter your surname"
-              value={values.surname}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.surname && touched.surname && "error"}
-            />
-            {errors.surname && touched.surname && (
-              <div className="input-feedback">{errors.surname}</div>
-            )}
+              <label htmlFor="surname">Surname: </label>
+              <input
+                name="surname"
+                type="text"
+                placeholder="Enter your surname"
+                value={values.surname}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.surname && touched.surname && "error"}
+              />
+              {errors.surname && touched.surname && (
+                <div className="input-feedback">{errors.surname}</div>
+              )}
 
-            <label htmlFor="comment">Comment: </label>
-            <input
-              name="comment"
-              type="text"
-              placeholder="Enter your comment"
-              value={values.comment}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={errors.comment && touched.comment && "error"}
-            />
-            {errors.comment && touched.comment && (
-              <div className="input-feedback">{errors.comment}</div>
-            )}
-            <button type="submit" disabled={isSubmitting}>
-              Add Review
-            </button>
-            {/* { <button type="button" disabled={isSubmitting} onClick={editReview}>
+              <label htmlFor="comment">Comment: </label>
+              <input
+                name="comment"
+                type="text"
+                placeholder="Enter your comment"
+                value={values.comment}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.comment && touched.comment && "error"}
+              />
+              {errors.comment && touched.comment && (
+                <div className="input-feedback">{errors.comment}</div>
+              )}
+              <button type="submit" disabled={isSubmitting}>
+                Add Review
+              </button>
+              {/* { <button type="button" disabled={isSubmitting} onClick={editReview}>
             Edit Review
           </button> } */}
-          </form>
-        );
-      }}
-    </Formik>
+            </form>
+          );
+        }}
+      </Formik>
+    </section>
   );
 }
 export default AddReview;
